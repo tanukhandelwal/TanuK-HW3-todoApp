ddf4485e9340c9309d72800aae779ef7a339d0d7
Tanu Khandelwal <tanukhandelwal87@gmail.com>
2015-02-06 11:21:41 -0800

    Initial Commit

---
+++ tanuK_HW3TodoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tanuK_HW3TodoApp
+//
+//  Created by Tanu Khandelwal on 2/6/15.
+//  Copyright (c) 2015 UW PCE learning. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ tanuK_HW3TodoApp/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  tanuK_HW3TodoApp
+//
+//  Created by Tanu Khandelwal on 2/6/15.
+//  Copyright (c) 2015 UW PCE learning. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
